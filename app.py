08b6d20863198a337e44d69db6c07a6e0966a96c
denis <123486371+BobColdd@users.noreply.github.com>
2026-09-04 22:05:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -756,14 +756,6 @@ def admin_matches():
 # ERROR HANDLERS
 # ============================================
 
-@app.errorhandler(404)
-def not_found(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
-
 # ============================================
 # START APP
 # ============================================
